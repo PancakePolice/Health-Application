705b7933083e0523bb1856c30130605a03f9434d
BuildTools <unconfigured@null.spigotmc.org>
2024-02-04 05:12:27 -0500

    Added BMR-related equations which deal with calories

--- Health Application.cpp
+++ Health Application.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 double WEIGHT = 0; // in pounds
 double HEIGHT = 0; // in inches
+double AGE = 0; // in years
 
 enum WeightStatus{UNDERWEIGHT, HEALTHY_WEIGHT, OVERWEIGHT, OBESITY};
 
 const WeightStatus getWeightStatus(double pounds = WEIGHT, double inches = HEIGHT);
 const double getBMI(double pounds = WEIGHT, double inches = HEIGHT);
-const double getIdealWeight(double inches = HEIGHT, double bmi = 21.7);
+const double getBMR(double pounds = WEIGHT, double inches = HEIGHT, int age = AGE);
+const double getIdealWeight(double bmi = 21.7, double inches = HEIGHT);
 
 int main() {
     if (WEIGHT == 0) {
@@ -22,8 +24,12 @@ int main() {
         cout << "Please enter your height in inches: ";
         cin >> HEIGHT;
     }
+    if (AGE == 0) {
+        cout << "Please enter your age in years: ";
+        cin >> AGE;
+    }
 
-    cout << "You have a BMI of " << setprecision(3) << getBMI(WEIGHT, HEIGHT) << " which is ";
+    cout << "You have a BMI of " << setprecision(3) << getBMI() << " which is ";
 
     switch (getWeightStatus()) {
     case UNDERWEIGHT:
@@ -47,23 +53,25 @@ int main() {
     
     switch (getWeightStatus()) {
     case UNDERWEIGHT:
-        cout << "You must gain a minimum of "  << getIdealWeight(HEIGHT, 25) - WEIGHT << " lbs to be at a healthy weight." << endl;
+        cout << "You must gain a minimum of "  << getIdealWeight(25) - WEIGHT << " lbs to be at a healthy weight." << endl;
         break;
     case HEALTHY_WEIGHT:
         cout << "Congratulations on being healthy weight!" << endl;
         break;
     case OVERWEIGHT:
-        cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(HEIGHT, 24.9) << " lbs to be at a healthy weight." << endl;
+        cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(24.9) << " lbs to be at a healthy weight." << endl;
         break;
     case OBESITY:
-       cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(HEIGHT, 29.9) << " lbs to no longer be obese." << endl;
-       cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(HEIGHT, 24.9) << " lbs to be at a healthy weight." << endl;
+       cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(29.9) << " lbs to no longer be obese." << endl;
+       cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(24.9) << " lbs to be at a healthy weight." << endl;
         break;
     default:
         cout << "ERROR." << endl;
     }
 
     // Calculate Ideal Calorie Reductions/Gains to Safely Reach Ideal Weight
+    cout << "You require " << setprecision(4) << getBMR() << " calories a day to maintain your current weight." << endl;
+    cout << "If you consume 1500 calories a day, you will lose " << setprecision(2) << ((getBMR() - 1500) * 7) / 3500 << " lbs a week." << endl;
 
     system("pause");
 
@@ -88,6 +96,12 @@ const double getBMI(double pounds, double inches) {
     return pounds / pow(inches, 2) * 703;
 }
 
-const double getIdealWeight(double inches, double bmi) {
+// The Mifflin-St Jeor Equation
+const double getBMR(double pounds, double inches, int age) {
+    double activityLevel = 1.2; // minimal activity (office job)
+    return ((10 * (WEIGHT / 2.205)) + (6.25 * (HEIGHT * 2.54)) - (5 * AGE) + 5) * activityLevel;
+}
+
+const double getIdealWeight(double bmi, double inches) {
     return bmi * pow(inches, 2) / 703;
 }
