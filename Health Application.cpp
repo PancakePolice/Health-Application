7dbc5389d3ef8b5011a53d7c77d4278697984c2b
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 00:47:46 -0500

    cleaned up the code to make it more readable

--- Health Application.cpp
+++ Health Application.cpp
@@ -2,79 +2,50 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 double WEIGHT = 0; // in pounds
 double HEIGHT = 0; // in inches
 double AGE = 0; // in years
 
-enum WeightStatus{UNDERWEIGHT, HEALTHY_WEIGHT, OVERWEIGHT, OBESITY};
+enum WeightStatus{ UNDERWEIGHT, HEALTHY_WEIGHT, OVERWEIGHT, OBESE} weightStatus;
+// enum menuStatus { BROWSING, QUIT } menuStatus;
 
 const WeightStatus getWeightStatus(double pounds = WEIGHT, double inches = HEIGHT);
 const double getBMI(double pounds = WEIGHT, double inches = HEIGHT);
-const double getBMR(double pounds = WEIGHT, double inches = HEIGHT, int age = AGE);
-const double getIdealWeight(double bmi = 21.7, double inches = HEIGHT);
+const double getBMR(double activityFactor = 1.2, double pounds = WEIGHT, double inches = HEIGHT, int age = AGE);
+const double getIdealWeight(double inches = HEIGHT);
+const string PrintWeightStatus();
+static const string PrintHealthyWeightRange(double inches = HEIGHT);
+static const double lb2kg(double pounds) { return pounds / 2.205; }
+static const double kg2lb(double mass) { return mass * 2.205; }
+static const double in2cm(double inches) { return inches * 2.54; }
 
 int main() {
-    if (WEIGHT == 0) {
-        cout << "Please enter your weight in pounds: ";
-        cin >> WEIGHT;
-    }
-    if (HEIGHT == 0) {
-        cout << "Please enter your height in inches: ";
-        cin >> HEIGHT;
-    }
-    if (AGE == 0) {
-        cout << "Please enter your age in years: ";
-        cin >> AGE;
-    }
-
-    cout << "You have a BMI of " << setprecision(3) << getBMI() << " which is ";
-
-    switch (getWeightStatus()) {
-    case UNDERWEIGHT:
-        cout << "underweight." << endl;
-        break;
-    case HEALTHY_WEIGHT:
-        cout << "healthy." << endl;
-        break;
-    case OVERWEIGHT:
-        cout << "overweight." << endl;
-        break;
-    case OBESITY:
-        cout << "obese." << endl;
-        break;
-    default:
-        cout << "ERROR." << endl;
+    if (!WEIGHT || !HEIGHT || !AGE) {
+        if (!WEIGHT) {
+            cout << "Please enter your weight in pounds: ";
+            cin >> WEIGHT;
+        }
+        if (!HEIGHT) {
+            cout << "Please enter your height in inches: ";
+            cin >> HEIGHT;
+        }
+        if (!AGE) {
+            cout << "Please enter your age in years: ";
+            cin >> AGE;
+        }
+        cout << "----------------------------------------" << endl;
     }
 
-    cout << "The ideal weight for your height is " << getIdealWeight() << " lbs." << endl;
-    cout << "This is a difference of " << abs(WEIGHT - getIdealWeight()) << " lbs." << endl;
-    
-    switch (getWeightStatus()) {
-    case UNDERWEIGHT:
-        cout << "You must gain a minimum of "  << getIdealWeight(25) - WEIGHT << " lbs to be at a healthy weight." << endl;
-        break;
-    case HEALTHY_WEIGHT:
-        cout << "Congratulations on being healthy weight!" << endl;
-        break;
-    case OVERWEIGHT:
-        cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(24.9) << " lbs to be at a healthy weight." << endl;
-        break;
-    case OBESITY:
-       cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(29.9) << " lbs to no longer be obese." << endl;
-       cout << "You must lose a minimum of " << WEIGHT - getIdealWeight(24.9) << " lbs to be at a healthy weight." << endl;
-        break;
-    default:
-        cout << "ERROR." << endl;
-    }
-
-    // Calculate Ideal Calorie Reductions/Gains to Safely Reach Ideal Weight
-    cout << "You require " << setprecision(4) << getBMR() << " calories a day to maintain your current weight." << endl;
-    cout << "If you consume 1500 calories a day, you will lose " << setprecision(2) << ((getBMR() - 1500) * 7) / 3500 << " lbs a week." << endl;
+    cout << "Body Mass Index: " << setprecision(3) << getBMI() << endl;
+    cout << PrintWeightStatus() << endl;
+    cout << "Ideal Weight: " << setprecision(3) << getIdealWeight() << " lbs" << endl;
+    cout << PrintHealthyWeightRange() << endl;
+    cout << "Base Metabolic Rate: " << setprecision(4) << getBMR() << " calories" << endl;
 
     system("pause");
-
     return 0;
 }
 
@@ -88,20 +59,44 @@ const WeightStatus getWeightStatus(double pounds, double inches) {
     if (bmi >= 25 && bmi <= 29.9)
         return OVERWEIGHT;
     if (bmi > 30)
-        return OBESITY;
+        return OBESE;
 }
 
-// https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html#Interpreted
+// https://www.cdc.gov/nccdphp/dnpao/growthcharts/training/bmiage/page5_2.html
 const double getBMI(double pounds, double inches) {
     return pounds / pow(inches, 2) * 703;
 }
 
 // The Mifflin-St Jeor Equation
-const double getBMR(double pounds, double inches, int age) {
-    double activityLevel = 1.2; // minimal activity (office job)
-    return ((10 * (WEIGHT / 2.205)) + (6.25 * (HEIGHT * 2.54)) - (5 * AGE) + 5) * activityLevel;
+const double getBMR(double activityFactor, double pounds, double inches, int age) {
+    return ((10 * lb2kg(WEIGHT)) + (6.25 * in2cm(HEIGHT)) - (5 * AGE) + 5) * activityFactor;
 }
 
-const double getIdealWeight(double bmi, double inches) {
-    return bmi * pow(inches, 2) / 703;
+// The J. D. Robinson Formula
+const double getIdealWeight(double inches) {
+    double idealWeight = 52; // in kilograms
+    for (int i = 60; i < HEIGHT; i++)
+        idealWeight += 1.9;
+    return kg2lb(idealWeight);
+}
+
+const string PrintWeightStatus() {
+    switch (getWeightStatus()) {
+    case UNDERWEIGHT:
+        return "You are underweight.";
+    case HEALTHY_WEIGHT:
+        return "You are a healthy weight.";
+    case OVERWEIGHT:
+        return "You are overweight.";
+    case OBESE:
+        return "You are obese.";
+    default:
+        return "Error getting weight status.";
+    }
+}
+
+static const string PrintHealthyWeightRange(double inches) {
+    int min = round(18.5 * pow(inches, 2) / 703);
+    int max = round(24.9 * pow(inches, 2) / 703);
+    return "Ideal Weight Range: " + to_string(min) + " - " + to_string(max) + " lbs.";
 }
